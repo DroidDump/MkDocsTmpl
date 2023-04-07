f8b0ef1fd09ae97497688a3e528d03fe312179e8
Aleksey Voko <voko.aleksey@gmail.com>
2023-04-07 04:57:55 +0400

    fix(app/main): fix 'MainActivity'

--- app/src/main/java/ru/voko/mk_docs_tmpl/MainActivity.kt
+++ app/src/main/java/ru/voko/mk_docs_tmpl/MainActivity.kt
@@ -1,11 +1,74 @@
 package ru.voko.mk_docs_tmpl
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import ru.voko.mk_docs_tmpl.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.webView.webViewClient = MyWebViewClient()
+
+        binding.webView.apply {
+            with(settings) {
+                this.javaScriptEnabled = true
+                this.builtInZoomControls = true
+                this.displayZoomControls = false
+            }
+        }
+
+        if (savedInstanceState == null) {
+            binding.webView.loadUrl("file:///android_asset/index.html")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.webView.saveState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.webView.restoreState(savedInstanceState)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && binding.webView.canGoBack()) {
+            binding.webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private inner class MyWebViewClient : WebViewClient() {
+
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            if (Uri.parse(request?.url.toString()).host
+                == Uri.parse("file:///android_asset/index.html").host
+            ) {
+                return false
+            }
+            Intent(Intent.ACTION_VIEW, Uri.parse(request?.url.toString())).apply {
+                startActivity(this)
+            }
+            return true
+        }
     }
 }
